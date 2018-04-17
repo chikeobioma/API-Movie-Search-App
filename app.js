40334cde2335c58b3aa659985dae6d297cc7ae47
EC2 Default User <ec2-user@ip-172-31-13-19.us-east-2.compute.internal>
2018-04-17 12:35:38 +0000

    first movie app deploy

---
+++ app.js
@@ -0,0 +1,80 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("home"); 
+});
+
+
+app.get("/searchResult", function(req, res){
+  var keyword = req.query.keyword;
+  var url = "http://www.omdbapi.com/?s="+keyword+"&apikey=thewdb"
+  request( url, function(error, response, body){
+    if (!error && response.statusCode == 200){
+      var searchResult = JSON.parse(body);
+    if ( searchResult.Search === undefined) {
+      res.render("errorPage");
+    } 
+      res.render("searchResult", {searchResult: searchResult});
+    } 
+    else {
+        console.log("Something went wrong");
+        console.log(error);
+    }
+  });
+});
+
+// app.listen(8080, '0.0.0.0', function(){
+//     console.log("The movie search app is now running...");
+// })
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The app is now running...");
+});
+
+//OR
+
+// var express = require("express");
+// var app = express();
+// var request = require("request");
+// var bodyParser = require("body-parser");
+
+// app.use(bodyParser.urlencoded({extended: true}));
+
+// app.set("view engine", "ejs");
+
+// app.post("/searchResult", function(req, res){
+//    var result = req.body.keyword;  
+//    var url = "http://www.omdbapi.com/?s="+keyword+"&apikey=thewdb"
+//    request( url, function(error, response, body){
+//     if(!error && response.statusCode == 200){
+//         var searchResult = JSON.parse(body);
+//         if ( searchResult.Search === undefined) {
+//               res.render("errorPage");
+//             }
+//     res.render("searchResult", {searchResult: searchResult});
+//     } 
+//     else {
+//         console.log("Something went wrong");
+//         console.log(error);
+//     }
+//   });
+// });
+
+// app.get("/", function(req, res){
+//   res.render("home"); 
+// });
+
+// app.get("/searchResult", function(req, res){
+//     res.render("searchResult");
+// });
+
+
+// app.listen(8080, '0.0.0.0', function(){
+//     console.log("The movie search app is now running...");
+// })
